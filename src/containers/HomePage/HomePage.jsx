ad76a49707e03e6b73df359c4e3fceb3fe91e9f5
Yoann LE PECH <yoann.le-pech@epitech.eu>
2022-05-14 14:40:22 +0200

    done filter and next page

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -1,31 +1,35 @@
 import { useState, useRef } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Dimensions, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { gql, useQuery } from '@apollo/client';
-import ModalSelector from 'react-native-modal-selector'
+import DropDownPicker from 'react-native-dropdown-picker';
 import CharacterCard from '../../components/CharacterCard';
-
 // TODO: Get all Rick and morty characters from API
 // TODO: Display all characters on screen, must be scrollable
 // TODO: Add search bar to filter characters by name, status (alive, dead, etc), species, type, gender
 
-const CharacterStatus = {
-  Alive: 'Alive',
-  Dead: 'Dead',
-  Unknown: 'Unknown',
-};
+const CharacterStatus = [
+  { label: '', value: '' },
+  { label: 'Alive', value: 'Alive' },
+  { label: 'Dead', value: 'Dead' },
+  { label: 'unknown', value: 'unknown' },
+]
 
-const CharacterGender = {
-  Female: 'Female',
-  Male: 'Male',
-  Genderless: 'Genderless',
-  Unknown: 'unknown',
-}; 
+const CharacterGender = [
+  { label: '', value: '' },
+  { label: 'Female', value: 'Female' },
+  { label: 'Male', value: 'Male' },
+  { label: 'Genderless', value: 'Genderless' },
+  { label: 'Unknown', value: 'unknown' },
+];
 
 
 const GET_CHARACTERS = gql`
-  query ($status: String, $gender: String) {
-    characters (filter: {status: $status, gender: $gender}) {
+  query ($status: String, $gender: String, $page: Int) {
+    characters (page: $page filter: {status: $status, gender: $gender}) {
+      info {
+        pages
+      }
       results {
         name
         id
@@ -39,22 +43,21 @@ const GET_CHARACTERS = gql`
   }
   `;
 
+  const windowWidth = Dimensions.get('window').width;
+  const windowHeight = Dimensions.get('window').height;
+
 const HomePage = () => {
-  const [status, setStatus] = useState();
-  const [gender, setGender] = useState();
-  const pickerRef = useRef();
+  const [status, setStatus] = useState('');
+  const [gender, setGender] = useState('');
+  const [openStatusList, setOpenStatusList] = useState(false);
+  const [openGenderList, setOpenGenderList] = useState(false);
+  const [statusItem, setStatusItems] = useState(CharacterStatus);
+  const [genderItem, setGenderItems] = useState(CharacterGender);
+  const [page, setPage] = useState(1);
 
   const { loading, error, data } = useQuery(GET_CHARACTERS, {
-    variables: { status, gender },
+    variables: { status, gender, page },
   });
-  
-  function open() {
-    pickerRef.current.focus();
-  }
-  
-  function close() {
-    pickerRef.current.blur();
-  }
 
   if (loading) {
     return (
@@ -73,15 +76,42 @@ const HomePage = () => {
   }
 
   console.log(loading, error, data);
-  
+
   return (
     <View style={styles.containerWrapper}>
       <SafeAreaView style={styles.container}>
+        <DropDownPicker
+          open={openStatusList}
+          value={status !== '' ? status : 'Select the Status'}
+          items={statusItem}
+          setOpen={setOpenStatusList}
+          setValue={setStatus}
+          setItems={setStatusItems}
+          placeholder="Select the status"
+          containerStyle={{ paddingBottom: 10 }}
+          onChangeValue={() => setPage(1)}
+        />
+        <DropDownPicker
+          open={openGenderList}
+          value={gender !== '' ? gender : 'Select the gender'}
+          items={genderItem}
+          setOpen={setOpenGenderList}
+          setValue={setGender}
+          setItems={setGenderItems}
+          placeholder="Select the gender"
+          onChangeValue={() => setPage(1)}
+          zIndex={1}
+        />
+        <View style={styles.content}>
         <ScrollView>
-        {data.characters.results.map(character => (
-         <CharacterCard key={character.id} data={character} />
-        ))}
+          {data.characters.results.map(character => (
+            <CharacterCard key={character.id} data={character} />
+          ))}
+          {data.characters.info.pages > page && (
+            <Button title="Next Page" onPress={() => setPage(page + 1)} />
+          )}
         </ScrollView>
+        </View>
       </SafeAreaView>
     </View>
   );
@@ -96,34 +126,10 @@ const styles = StyleSheet.create({
   container: {
     paddingStart: 20,
     paddingEnd: 20,
-  }
+  },
+  content: {
+   height: windowHeight - 200,
+  },
 });
 
 export default HomePage;
-
-
-// fetch('https://rickandmortyapi.com/graphql', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify({
-//     query: `
-//         {
-//           characters {
-//                  results {
-//                    name
-//                    id
-//                    type
-//                    status
-//                    species
-//                    gender
-//                    image
-//                  }
-//                }
-//         }
-//     `,
-//   }),
-// })
-//   .then(res => res.json())
-//   .then(res => console.log(res.data.characters.results))
